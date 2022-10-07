6096a4a15e2bc354128ec9fdb8f1868186fb89d3
Urmo Võsa <urmo.vosa@gmail.com>
2022-10-07 10:15:49 +0300

    Add MAC filter

--- bin/make_inclusion_lists.R
+++ bin/make_inclusion_lists.R
@@ -22,6 +22,8 @@ HWE_thresh <- 1e-6
 MACH_R2_thresh <- 0.4
 CALL_thresh <- 1
 
+MAC_thresh <- 0
+
 # You can adjust the gene QC thresholds here
 gene_uniq_thresh <- 0.8
 
@@ -35,41 +37,53 @@ exp_pcs <- 20
 # Shall we remove those expression PCs which are under genetic regulation? By default not.
 gen_reg_pcs <- FALSE
 
+
 ####################################
 
-# Make SNP QC files
+# Make SNP inclusion files
 fileConn<-file(paste0(outputpath, "/covariate_indices.txt"), "w")
 
 sink(paste0(outputpath, "/filter_logs.log"))
-cat("Dataset\tN\tNr. SNPs\tFiltered SNPs\tTyped SNPs\tSNPs AC>5; not filtered\tGenes original\tGenes filtered\tGenetically regulated PCs\n")
+cat("Dataset\tN\tNr. SNPs\tFiltered SNPs\tTyped SNPs\tSNPs MAC>10; not filtered\tSNPs AC>5; not filtered\tGenes original\tGenes filtered\tGenetically regulated PCs\n")
 
 for (dataset in dataset_path){
 
 dataset_name <- str_replace(as.character(fs::dir_ls(path = paste0(dataset, "/SumStats/genotypes/SNPQC/"), glob = "*_SNPQC.txt.gz")), ".*/SNPQC/", "")
 dataset_name <- str_replace(dataset_name, "_SNPQC.txt.gz", "")
 
-message(paste0("Processing: ", dataset_name))
-message("Make SNP QC files")
+message(paste0("Processing ", dataset_name))
+message("Make SNP inclusion files")
 
 snpqc_filename <- fs::dir_ls(path = paste0(dataset, "/SumStats/genotypes/SNPQC/"), glob = "*_SNPQC.txt.gz")
-snpqc <- fread(snpqc_filename, showProgress = FALSE)
+snpqc <- fread(snpqc_filename)
+
+# fix initial filter MAF>0.01
+snpqc <- snpqc[MAF > 0.01]
 
 # filters
 orig_snps <- nrow(snpqc)
 snpqc <- snpqc[MAF > MAF_thresh & HWE > HWE_thresh & MACH_R2 > MACH_R2_thresh & CALL == CALL_thresh]
 
-# Show variants which have at least 5 allele counts for rarer genotype
+# Count variants which have at least 5 allele counts for rarer genotype
 hom1_gen_counts <- str_replace(snpqc$Genotype_Counts, ", .*", "")
 snpqc$hom1_gen_counts <- as.numeric(str_replace(hom1_gen_counts, ".*: ", ""))
 
 hom2_gen_counts <- str_replace(snpqc$Genotype_Counts, ".*: ", "")
 snpqc$hom2_gen_counts <- as.numeric(str_replace(hom2_gen_counts, ",", ""))
 
+het_gen_counts <- sub('(,.*?),.*', '\\1', snpqc$Genotype_Counts)
+snpqc$het_gen_counts <- as.numeric(str_replace(het_gen_counts, ".* ", ""))
+
+# Calculate MAC for each SNP
+mac <- snpqc[, .(MAC = min(c(hom1_gen_counts, hom2_gen_counts)) + 2 * het_gen_counts), by = .(ID)]
+
 # Calculate N
 N_split <- unlist(str_split(snpqc$Genotype_Counts[1], ", "))
 N_split <- str_replace(N_split, ".*: ", "")
 N_split <- as.numeric(str_replace(N_split, ",", ""))
 N_split <- sum(N_split)
+
+snpqc <- snpqc[ID %in% mac[MAC > MAC_thresh]$ID]
 
 fwrite(data.table(ID = snpqc$ID), paste0(outputpath, "/", dataset_name, "_SnpsToInclude.txt"), sep = "\t", row.names = FALSE)
 
@@ -80,7 +94,7 @@ fwrite(data.frame(ID = genes_to_include), paste0(outputpath, "/", dataset_name, 
 message("Check genetically regulated expression PCs")
 # Report PCs under genetic regulation, make covariate indices file
 genetic_pcs <- fread(paste0(dataset, "/", dataset_name, "_GenRegPcs.txt.gz"))
-genetic_pcs <- genetic_pcs[SNP %in% snpqc$ID & `P-value` < GenRegPC_thresh]
+genetic_pcs <- genetic_pcs[`P-value` < GenRegPC_thresh]
 genetic_pcs <- mixedsort(unique(genetic_pcs$Phenotype))
 
 # Identify which covariates to add
@@ -101,7 +115,8 @@ cat(paste(dataset_name,
 N_split, 
 orig_snps,
 nrow(snpqc), 
-nrow(snpqc[snpqc$TYPED == "1",]), 
+nrow(snpqc[snpqc$TYPED == "1", ]),
+nrow(mac[MAC > 10]),
 nrow(snpqc[hom1_gen_counts > 5 & hom2_gen_counts > 5]),
 nrow(geneqc_processed),
 length(genes_to_include),
